6284f76769913cde30522a44626fe54c060e5404
Nazli Zamanian <nazlizamanian@gmail.com>
2023-10-24 21:02:35 +0200

    new changes

---
+++ Labs/lab1.py
@@ -0,0 +1,6 @@
+#print("Hello, world!")
+
+#1.3.1 Test questions on number
+#1 
+
+result= 15*"spam "
--- Labs/lab2.py
+++ Labs/lab2.py
@@ -11,12 +11,10 @@ with open("score2.txt", 'r') as file:
             name = f"{elements[2]} {elements[3]}"  # Combine first name and last name
             points = int(elements[4])  # Extract points as an integer
 
-            # Update the points for the person in the dictionary
             if name in pointsPerPerson:
                 pointsPerPerson[name] += points
             else:
                 pointsPerPerson[name] = points
-
 
 max_points = max(pointsPerPerson.values())
 top_scorers = [name for name, points in pointsPerPerson.items() if points == max_points]
