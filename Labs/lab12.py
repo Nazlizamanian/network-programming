5372005717e7a33ca7d46b3b5a3796689ff85eff
Nazli Zamanian <nazlizamanian@gmail.com>
2023-11-04 13:16:14 +0100

    commit

--- Labs/lab12.py
+++ Labs/lab12.py
@@ -1,1 +1,27 @@
 #12 Nazli Zamanian Gustavsson
+
+import firebase_admin 
+from firebase_admin import  credentials, db 
+
+cred = credentials.Certificate("path/to/serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://lab12-9e351-default-rtdb.europe-west1.firebasedatabase.app/'})  
+
+# Create a reference to the root of your database
+ref = db.reference('/')
+
+def stream_handler(message):
+    if message['event_type'] == 'put':
+        if message['path'] == '/message':
+            for key, value in message['data'].items():
+                handle_message(value)
+                
+def handle_message(message):
+    name = message['name']
+    text = message['text']
+    print(f'{name}: {text}')
+    
+message_stream = ref.child('messages').listen(stream_handler)
+
+new_message = {'name': 'Jojo', 'text': 'hello, Firebase!'}
+ref.child('messages').push(new_message)
+            
--- Labs/lab9.py
+++ Labs/lab9.py
@@ -1,1 +1,19 @@
 #Lab9 Nazli Zamanian Gustavsson
+#9.2 Comporession
+
+file_path = 'exempeltext.txt'
+
+with open(file_path, 'rb') as file:
+    bytes_content = file.read()
+    txt = bytes_content.decode("utf-8", errors= "replace")
+    
+byteArr = bytearray(txt, "utf-8")
+    
+number_of_symbols= len(txt)
+    
+number_of_bytes= len(byteArr)
+    
+print(f"Number of symbols in the string: {number_of_symbols}")
+print(f"Number of bytes in the byte array: {number_of_bytes}")
+#number_of_symbols in the stirngs are: 29189
+#number_of_bytes in the byte array are: 31787
