cfe68ce4863e3053d9c1f8a8f017591e21a9ef3b
Nazli Zamanian <nazlizamanian@gmail.com>
2023-11-05 23:26:16 +0100

    Changed structure

--- Labs/lab5.py
+++ Labs/lab5.py
@@ -1,96 +1,73 @@
 #Lab5 Nazli Zamanian Gustavsson
 import socket
 
+def serversideGetPlaySocket():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('0.0.0.0', 60003))
+    server_socket.listen(1)
+    
+    print("Waiting for a client to connect...")
+    client_socket, addr = server_socket.accept()
+    print(f"Connected to {addr}")
+    
+    return server_socket, client_socket
+
+def clientsideGetPlaySocket(host):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, 60003))
+    
+    return client_socket
+
 def main():
     role = input("Do you want to be a client (C) or server (S)? ").strip().upper()
     
     if role == 'S':
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.bind(('localhost', 60003)) 
-        server_socket.listen(1)
-
-        print("Waiting for a client to connect...")
-        client_socket, addr = server_socket.accept() #server set ut to accept incoming c
-        print(f"Connected to {addr}")
-
-        player_points = 0
-        opponent_points = 0
-
-        while True:
-            print(f"({player_points},{opponent_points}) Your move: ", end='', flush=True)
-            player_move = input().strip().upper()
-
-            while player_move not in ('R', 'P', 'S'):
-                print("Invalid move. Please enter R, P, or S: ", end='', flush=True)
-                player_move = input().strip().upper()
-
-            client_socket.send(player_move.encode('utf-8')) #sends clients move.
-
-            opponent_move = client_socket.recv(1).decode('utf-8')
-            print(f"(opponent's move: {opponent_move})")
-
-            result = determine_winner(player_move, opponent_move)
-
-            if result == "win":
-                print("You won this round!")
-            elif result == "lose":
-                print("Opponent won this round!")
-
-            # Update points
-            if result == "win":
-                player_points += 1
-            elif result == "lose":
-                opponent_points += 1
-
-            print(f"({player_points},{opponent_points})")
-
-            if check_game_over(player_points, opponent_points):
-                break
-
-        client_socket.close()
-        server_socket.close()
-
+        server_socket, client_socket = serversideGetPlaySocket()
     elif role == 'C':
-        # Client code
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-        server_ip = input("Enter the server's IP address: ")  
-        client_socket.connect((server_ip, 60003))
-
-        player_points = 0
-        opponent_points = 0
-
-        while True:
-            player_move = input(f"({player_points},{opponent_points}) Your move: ").strip().upper()
-
-            while player_move not in ('R', 'P', 'S'):
-                print("Invalid move. Please enter R, P, or S.")
-                player_move = input().strip().upper()
-
-            client_socket.send(player_move.encode('utf-8'))
-
-            opponent_move = client_socket.recv(1).decode('utf-8')
-            print(f"(opponent's move: {opponent_move})")
-            
-            result = determine_winner(player_move, opponent_move)
-
-            if result == "win":
-                print("You won this round!")
-            elif result == "lose":
-                print("Opponent won this round!")
-
-            # Update points
-            if result == "win":
-                player_points += 1
-            elif result == "lose":
-                opponent_points += 1
-
-            if check_game_over(player_points, opponent_points):
-                break
-
+        host = input("Enter the server's IP address: ")
+        client_socket = clientsideGetPlaySocket(host)
     else:
         print("Invalid role. Please enter 'C' or 'S.")
-        
-def determine_winner(player, opponent): #Game Logic
+        return
+
+    player_points = 0
+    opponent_points = 0
+
+    while True:
+        print(f"({player_points},{opponent_points}) Your move: ", end='', flush=True)
+        player_move = input().strip().upper()
+
+        while player_move not in ('R', 'P', 'S'):
+            print("Invalid move. Please enter R, P, or S: ", end='', flush=True)
+            player_move = input().strip().upper()
+
+        client_socket.send(player_move.encode('utf-8'))
+
+        opponent_move = client_socket.recv(1).decode('utf-8')
+        print(f"(opponent's move: {opponent_move})")
+
+        result = determine_winner(player_move, opponent_move)
+
+        if result == "win":
+            print("You won this round!")
+        elif result == "lose":
+            print("Opponent won this round!")
+
+        if result == "win":
+            player_points += 1
+        elif result == "lose":
+            opponent_points += 1
+
+        print(f"({player_points},{opponent_points})")
+
+        if check_game_over(player_points, opponent_points):
+            break
+
+    client_socket.close()
+    if role == 'S':
+        server_socket.close()
+
+def determine_winner(player, opponent):
     if player == opponent:
         return "draw"
     elif player == 'R' and opponent == 'S':
@@ -101,9 +78,9 @@ def determine_winner(player, opponent): #Game Logic
         return "win"
     else:
         return "lose"
-    
-def check_game_over(player_points, opponent_points): #Game rules, first to 3. 
-    if player_points == 3 or opponent_points == 3:
+
+def check_game_over(player_points, opponent_points):
+    if player_points == 10 or opponent_points == 10:
         print("Game over!")
         if player_points > opponent_points:
             print(f"You won {player_points} against {opponent_points}")
@@ -111,6 +88,6 @@ def check_game_over(player_points, opponent_points): #Game rules, first to 3.
             print(f"You lost {player_points} against {opponent_points}")
         return True
     return False
-    
+
 if __name__ == "__main__":
     main()
