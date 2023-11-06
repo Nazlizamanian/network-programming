e435abd4772b8991c1f9c72da3923221a5ca901b
Nazli Zamanian <nazlizamanian@gmail.com>
2023-11-06 17:06:14 +0100

    Committs

--- Labs/lab11.py
+++ Labs/lab11.py
@@ -1,66 +1,79 @@
-#Lab 11 Nazli Zamanian Gustavsson
 import sqlite3
 
-data_file = 'score2.txt'
-
-persons_data = []
-scores_data = []
-
-with open(data_file, 'r') as file:
-    for line in file:
-        # Assuming format: identity, first name, last name, points
-        identity, first_name, last_name, points = line.strip().split(',')
-        persons_data.append((identity, first_name, last_name))
-        scores_data.append((points,))
-
-# Step 2: Create an SQLite database and define tables
-db_file = 'lab11Database.db'
-
-conn = sqlite3.connect(db_file)
+# Create a SQLite database and establish a connection
+conn = sqlite3.connect('score_data.db')
 cursor = conn.cursor()
 
-# Create "persons" table with first name and last name
-cursor.execute('''CREATE TABLE IF NOT EXISTS persons (
-                  identity TEXT PRIMARY KEY,
-                  first_name TEXT,
-                  last_name TEXT)''')
+# Create the 'persons' and 'scores' tables
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS persons (
+        id INTEGER PRIMARY KEY,
+        first_name TEXT,
+        last_name TEXT
+    )
+''')
 
-# Create "scores" table with points
-cursor.execute('''CREATE TABLE IF NOT EXISTS scores (
-                  points INTEGER)''')
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS scores (
+        task_number INTEGER,
+        points INTEGER,
+        person_id INTEGER,
+        FOREIGN KEY (person_id) REFERENCES persons(id)
+    )
+''')
 
+# Read data from the file and insert it into the tables
+with open('score2.txt', 'r') as file:
+    for line in file:
+        parts = line.split()
+        task_number = int(parts[1])
+        first_name = parts[2]
+        last_name = parts[3]
+        points = int(parts[4])
+        
+        cursor.execute('INSERT INTO persons (first_name, last_name) VALUES (?, ?)', (first_name, last_name))
+        
+        cursor.execute('SELECT id FROM persons WHERE first_name=? AND last_name=?', (first_name, last_name))
+        person_id = cursor.fetchone()[0]
+        
+        cursor.execute('INSERT INTO scores (task_number, points, person_id) VALUES (?, ?, ?)', (task_number, points, person_id))
 
-cursor.executemany("INSERT INTO persons VALUES (?, ?, ?)", persons_data)
-cursor.executemany("INSERT INTO scores VALUES (?)", scores_data)
+conn.commit()
+conn.close()
 
+# Reopen the database and connection
+conn = sqlite3.connect('score_data.db')
+cursor = conn.cursor()
 
 # (a) List the 10 persons with the highest total points
 cursor.execute('''
-    SELECT p.first_name, p.last_name, SUM(s.points) as total_points
+    SELECT p.first_name, p.last_name, SUM(s.points) AS total_points
     FROM persons p
-    JOIN scores s ON p.identity = s.identity
-    GROUP BY p.identity
+    JOIN scores s ON p.id = s.person_id
+    GROUP BY p.id
     ORDER BY total_points DESC
     LIMIT 10
 ''')
-
-highest_total_points = cursor.fetchall()
-print("(a) List of 10 persons with the highest total points:")
-for row in highest_total_points:
-    print(row)
+top_10_persons = cursor.fetchall()
 
 # (b) List the 10 most difficult tasks (minimal total points)
 cursor.execute('''
-    SELECT s.points
-    FROM scores s
-    ORDER BY s.points
+    SELECT task_number, SUM(points) AS total_points
+    FROM scores
+    GROUP BY task_number
+    ORDER BY total_points
     LIMIT 10
 ''')
-
 most_difficult_tasks = cursor.fetchall()
-print("\n(b) List of 10 most difficult tasks (minimal total points):")
-for row in most_difficult_tasks:
-    print(row)
 
-conn.commit()
+
+print("(a) List of the 10 persons with the highest total points: ")
+for person in top_10_persons:
+    print(f"{person[0]} {person[1]} - Total Points: {person[2]}")
+
+print("\n(b) List of the 10 most difficult tasks: ")
+for task in most_difficult_tasks:
+    print(f"Task {task[0]} - Total Points: {task[1]}")
+
+# Close the connection
 conn.close()
--- Labs/lab6.py
+++ Labs/lab6.py
@@ -1,39 +1,90 @@
 # #Lab6 Nazli Zamanian Gustavsson
-
 import socket
+#http://localhost:8080/blub/blib.html. 
+#http://localhost:8080/
 
-def handle_client(client_socket):
-    request = client_socket.recv(1024).decode('utf-8')
+def create_response_html(request):
+    response = """
+HTTP/1.1 200 OK
+Content-Type: text/html
+Content-Length: {}
 
-    if request:
-        # Prepare the response headers
-        response = "HTTP/1.1 200 OK\r\n"
-        response += "Content-Type: text/html\r\n\r\n"
+<html>
+<head>
+    <title>Request Description</title>
+</head>
+<body>
+    <h1>Your browser made the following request: </h1>
+    <pre>{}</pre>
+</body>
+</html>
+""".format(len(request), request)
+
+    return response
+
+def start_server(host, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind((host, port))
+        server_socket.listen()
+
+        print(f"Server listening on {host}:{port}")
+
+        while True:
+            client_socket, client_address = server_socket.accept()
+            with client_socket:
+                print(f"Connection from {client_address}")
+
+                data = client_socket.recv(1024).decode('utf-8')
+                if not data:
+                    break
+
+                # Extract the request from the client's data
+                request = data.split('\n\n', 1)[-1]
+
+                response = create_response_html(request)
+
+                client_socket.sendall(response.encode('utf-8'))
+
+if __name__ == '__main__':
+    host = '127.0.0.1' 
+    port = 8080 
+
+    start_server(host, port)
+
+
+########################################################################
+# def handle_client(client_socket):
+#     request = client_socket.recv(1024).decode('utf-8')
+
+#     if request:
+#         # Prepare the response headers
+#         response = "HTTP/1.1 200 OK\r\n"
+#         response += "Content-Type: text/html\r\n\r\n"
         
-        # Create an HTML response with the client's request
-        response += "<html><pre>"
-        response += "<h1 style='font-family: Times New Roman'> Your browser sent the following request: </h1>"
-        response += request 
-        response += "</pre></html>"
+#         # Create an HTML response with the client's request
+#         response += "<html><pre>"
+#         response += "<h1 style='font-family: Times New Roman'> Your browser sent the following request: </h1>"
+#         response += request 
+#         response += "</pre></html>"
 
-        # Send the response to the client
-        client_socket.send(response.encode('utf-8'))
+#         # Send the response to the client
+#         client_socket.send(response.encode('utf-8'))
     
-    client_socket.close()
+#     client_socket.close()
 
-def main():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('localhost', 8080))
-    server.listen(3)  
+# def main():
+#     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     server.bind(('localhost', 8080))
+#     server.listen(3)  
 
-    print("Listening on http://localhost:8080/blub/blib.html.")
+#     print("Listening on http://localhost:8080/blub/blib.html.")
 
-    while True:
-        client_socket, client_address = server.accept()
-        print(f"Accepted connection from {client_address}")
-        handle_client(client_socket)
+#     while True:
+#         client_socket, client_address = server.accept()
+#         print(f"Accepted connection from {client_address}")
+#         handle_client(client_socket)
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
 
 
