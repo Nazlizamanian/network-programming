694d8415946b12ef2264adf7c7039a31e1c83bc2
Nazli Zamanian <nazlizamanian@gmail.com>
2023-11-13 14:54:31 +0100

    commit

--- Labs/lab10.py
+++ Labs/lab10.py
@@ -1,1 +1,50 @@
-#Lab10 Nazli Zamanian Gustavsson
+#Lab10 Nazli Zamanian Gustavsson Regex
+
+
+#10.2 Introductory experiments
+import re
+txt = "Hanoror bought 5 portions of orange soil for 13.50 EUR."
+re.findall("or", txt)
+
+#10.2.1 Dot means joker
+#dot in regex matches all characters. except linebreak.
+import re
+txt = "Hanoror bought 5 portions of orange soil for 13.50 EUR."
+re.findall(".", txt)
+
+import re
+txt = "Hanoror bought 5 portions of orange soil for 13.50 EUR."
+re.findall("or.", txt) #searches for all strings that start with or and continue charater.
+# findall find only non-overlapping strings.
+
+re.findall("..\.",txt) #gives us a real . 
+
+#10.2.3 Other special characters
+re.findall(r"\w",txt) #w as in word, matches all words. 
+re.findall(r"\W",txt)#!= as w, w as not word. 
+
+re.findall(r"\d",txt) #digit
+re.findall(r"\D",txt)# opposite gives word. 
+
+re.findall(r"\s",txt) #spaces 
+
+
+
+#Task 1 
+
+def is_valid_email(email):
+    # Define the regular expression pattern for a valid email address
+    email_pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    
+    # Use re.match() to check if the email matches the pattern
+    match = re.match(email_pattern, email)
+    
+    # If there is a match, the email is valid; otherwise, it's not
+    return bool(match)
+
+# Test the function with examples
+print(is_valid_email('r.nohre@jth.hj.se'))  
+print(is_valid_email('bjox@se'))      
+print(is_valid_email('adam@example.com'))  
+print(is_valid_email('jox@jox@jox.com."'))    
+
